43572dcf0a70fd33a753c01a1d7979ec154c85e2
comtrial <comtrial97@gmail.com>
2023-03-30 20:06:15 +0900

    feat: project setup

---
+++ src/test/java/JPABook/JPAShop/JpaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package JPABook.JPAShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
